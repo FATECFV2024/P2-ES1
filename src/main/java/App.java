dbd96a484c5f4a36e156814713d1091cca6068d1
Francisco Douglas <98854868+franciscodouglas@users.noreply.github.com>
2024-12-05 22:42:36 -0300

    init commit

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class App {
+
+
+    public static void fila(Scanner scanner) {
+        //IMPLEMENTE AQUI A LÓGICA PARA A FILA
+    }
+
+    public static void lista(Scanner scanner) {
+        //IMPLEMENTE AQUI A LÓGICA PARA A LISTA
+    }
+
+    public static void main(String[] args) throws Exception {
+        //não modifique o método main
+        Scanner scanner = new Scanner(System.in);
+        fila(scanner);
+        lista(scanner);
+        scanner.close();
+    }
+}
